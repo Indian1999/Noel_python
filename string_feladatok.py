12aa621d515ac76fae072ef89f457c9d9797a713
Indian1999 <haszab99@gmail.com>
2025-10-15 19:02:56 +0200

    feladatok

---
+++ string_feladatok.py
@@ -0,0 +1,11 @@
+# Olvassunk be és tároljunk el egy szöveget
+
+# Írjuk ki hogy hány karakterből el a beolvasott szöveg
+
+# Írjuk ki a szöveget visszafelé
+
+# Hány magánhangzó szerepel a szövegben?
+
+# Hanyadik indexű karakter az első 'a' betű a szövegben? Ha nincs, akkor -1-et írjunk ki
+
+# Összetett feladat:
