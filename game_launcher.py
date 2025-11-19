fd6192c811cddae7b996a4e978e20d491474acb4
Indian1999 <haszab99@gmail.com>
2025-11-19 19:08:39 +0100

    update

--- game_launcher.py
+++ game_launcher.py
@@ -11,4 +11,14 @@ print("5: Factorio")
 answer = input("Add meg az indítani kívánt játék számát!\n")
 
 if answer == "1":
-    master_mind()
+    difficulty = input("Hanyas nehézségen akarsz játszani? (1-5)\n")
+    if difficulty == "1":
+        master_mind(1, 5, 4)
+    elif difficulty == "2":
+        master_mind(1, 7, 4)
+    elif difficulty == "3":
+        master_mind(1, 9, 4)
+    elif difficulty == "4":
+        master_mind(1, 9, 5)
+    elif difficulty == "5":
+        master_mind(1, 9, 6)
--- szinozon.py
+++ szinozon.py
@@ -1,15 +1,15 @@
 import random
 
-def generate_puzzle():
+def generate_puzzle(lower_bound = 1, upper_bound = 6, number_of_digits = 4):
     puzzle = ""  
-    while len(puzzle) < 4:
-        num = str(random.randint(1, 6))
+    while len(puzzle) < number_of_digits:
+        num = str(random.randint(lower_bound, upper_bound))
         if num not in puzzle:
             puzzle += num 
     return puzzle # A függvény visszatérési értéke
 
-def ask_player(puzzle):
-    tipp = input("Adj meg négy számjegyet (1-6) szóközek nélkül (pl.: 2134)!\n")
+def ask_player(puzzle, lower_bound = 1, upper_bound = 6, number_of_digits = 4):
+    tipp = input(f"Adj meg négy számjegyet ({lower_bound}-{upper_bound}) szóközek nélkül (pl.: {generate_puzzle(lower_bound, upper_bound, number_of_digits)})!\n")
     jo_szamok_rossz_helyen = 0
     jo_szamok_jo_helyen = 0
     for i in range(len(tipp)): # i = 0, 1, 2, 3
@@ -17,19 +17,19 @@ def ask_player(puzzle):
             jo_szamok_jo_helyen += 1
         elif tipp[i] in puzzle:
             jo_szamok_rossz_helyen += 1
-    if jo_szamok_jo_helyen == 4:
+    if jo_szamok_jo_helyen == number_of_digits:
         return False
     else:
         print("Jó számok jó helyen:", jo_szamok_jo_helyen)
         print("Jó számok, de rossz helyen:", jo_szamok_rossz_helyen)
         return True
 
-def master_mind():
-    puzzle = generate_puzzle()
+def master_mind(lower_bound = 1, upper_bound = 6, number_of_digits = 4):
+    puzzle = generate_puzzle(lower_bound, upper_bound, number_of_digits)
     gameOn = True
     tippek = 0
     while gameOn:
-        gameOn = ask_player(puzzle)
+        gameOn = ask_player(puzzle, lower_bound, upper_bound, number_of_digits)
         tippek += 1
     print(f"Szép volt! {tippek} lépésből rájöttél, hogy a megoldás {puzzle} volt.")
 
